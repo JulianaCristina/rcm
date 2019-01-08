457beb9cb54f3113a3681b860f434ea1c65dbe69
Juliana-PC\Juliana <julianacristina780@gmail.com>
2019-01-08 09:40:16 -0200

    Página prêmio do cerrado mineiro

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>VI Prêmio - Região do Cerrado Mineiro</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" type="image/png" href="images/favicon.png"/>	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 imagesDestaque">
+				<div class="img-responsive logo">
+					<img src="images/RCM_landing_logo1.png" alt="">
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 imagesDestaque">
+				<div class="img-responsive tag">
+					<img src="images/RCM_landing_tagline1.png" alt="">
+				</div>
+			</div>
+			
+		</div>
+		<div class="row">
+			<div class="col-xs-12 col-md-12 col-lg-12 texto1 central">
+				<p class="text-center"> Saiba o que mudou para a VI edição do</p>
+				<p class="text-center">Prêmio da Região do Cerrado Mineiro.</p>
+				<div class="linha col-xs-12"></div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-4 col-xs-offset-2 col-sm-2 col-sm-offset-2  icone esq">
+				<img src="images/1.png" alt="" class="img-responsive">
+			</div>
+			<div class="col-xs-4 col-sm-2  icone dir" >
+				<img src="images/2.png" alt="" class="img-responsive">
+			</div>
+			<div class="col-xs-4 col-xs-offset-2 col-sm-2 col-sm-offset-0 icone esq">
+				<img src="images/3.png" alt="" class="img-responsive">
+			</div>
+			<div class="col-xs-4 col-sm-2 icone dir">
+				<img src="images/4.png" alt="" class="img-responsive">
+			</div>
+			
+		</div>
+		<div class="row">
+			<div class="col-xs-12 col-md-12 col-lg-12 texto1 botao">
+				<p class="text-center envio">Envio de amostras até 12/09/2018</p>
+				<button type="button" class="btn btn-md baixar " onclick="window.location.href='regulamento_vi_premio_regiao_cerrado_mineiro_2018.pdf';"> Baixe o Regulamento</button>
+			</div>
+		</div>
+		
+		
+	</div>
+</body>
+</html>
